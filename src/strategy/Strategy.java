b2d6675570dcc61d7967f25075cfb19aaa6e6069
chenbin <chenbin!@#$>
2020-02-21 18:17:28 +0800

    设计模式

---
+++ src/strategy/Strategy.java
@@ -0,0 +1,6 @@
+package strategy;
+
+public  abstract  class Strategy {
+    public abstract  void metod1();
+    public abstract  void metod2();
+}
---
+++ src/strategy/StrategyA.java
@@ -0,0 +1,13 @@
+package strategy;
+
+public class StrategyA extends Strategy {
+    @Override
+    public void metod1() {
+        System.out.println("学习算法");
+    }
+
+    @Override
+    public void metod2() {
+        System.out.println("看电影");
+    }
+}
